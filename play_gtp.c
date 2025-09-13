360310f47ec8bf74c78be70052a6632167312f54
Kazuma Arino <hogeika2@gmail.com>
2025-09-13 20:04:21 +0900

    Fix clear_board, support list_commands.

--- play_gtp.c
+++ play_gtp.c
@@ -35,6 +35,8 @@ DECLARE(gtp_reg_genmove);
 DECLARE(gtp_genmove);
 DECLARE(gtp_play);
 DECLARE(gtp_clear_board);
+DECLARE(gtp_list_commands);
+DECLARE(gtp_debug);
 DECLARE(gtp_quit);
 
 static struct gtp_command commands[] = {
@@ -44,6 +46,8 @@ static struct gtp_command commands[] = {
   {"reg_genmove",             gtp_reg_genmove},
   {"genmove",             gtp_genmove},
   {"play",            	      gtp_play},
+  {"list_commands",           gtp_list_commands},
+  {"debug",            	      gtp_debug},
   {"quit",             	      gtp_quit},
   {NULL,                      NULL}
 };
@@ -206,14 +210,22 @@ static int
 gtp_clear_board(char *s)
 {
   for(int i = 0; i < MAX_BOARD; i++) {
-    memset(&p[i][0], 0, MAX_BOARD);
-    memset(&l[i][0], 0, MAX_BOARD);
-    memset(&ma[i][0], 0, MAX_BOARD);
-    memset(&ml[i][0], 0, MAX_BOARD);
-    memset(&plast[i][0], 0, MAX_BOARD);
-    memset(&worm[i][0], 0, MAX_BOARD);
-    memset(&drag[i][0], 0, MAX_BOARD);
+    memset(&p[i][0], 0, sizeof(int)*MAX_BOARD);
+    memset(&l[i][0], 0, sizeof(int)*MAX_BOARD);
+    memset(&ma[i][0], 0, sizeof(int)*MAX_BOARD);
+    memset(&ml[i][0], 0,sizeof(int)* MAX_BOARD);
+    memset(&worm[i][0], 0, sizeof(int)*MAX_BOARD);
+    memset(&drag[i][0], 0, sizeof(int)*MAX_BOARD);
+
+    for(int j = 0; j < MAX_BOARD; j++) {
+      memset(&plast[i][j][0], 0, sizeof(int)*2);
+    }
   }
+
+  memset(libi, 0, sizeof(int)*MAXLIBS);
+  memset(libj, 0, sizeof(int)*MAXLIBS);
+  memset(adji, 0, sizeof(int)*100);
+  memset(adjj, 0, sizeof(int)*100);
 
   for (int i = 0; i < 9; i++)
      opn[i] = 1;
@@ -224,6 +236,8 @@ gtp_clear_board(char *s)
   pass = 0;
   mik = -1; mjk = -1;
   uik = -1; ujk = -1;
+  lib = 0;
+  openover = 0;
 
   return gtp_success("");
 }
@@ -248,6 +262,31 @@ gtp_genmove(char *s)
 
   gtp_start_response(GTP_SUCCESS);
   gtp_print_vertex(i, j);
+  return gtp_finish_response();
+}
+
+static int
+gtp_list_commands(char *s)
+{
+  gtp_start_response(GTP_SUCCESS);
+  for(int i = 0; commands[i].name != NULL; i++) {
+      gtp_printf("%s\n", commands[i].name);
+
+  }  
+  return gtp_finish_response();
+}
+
+
+static int
+gtp_debug(char *s)
+{
+  gtp_start_response(GTP_SUCCESS);
+  for(int row = 0; row < board_size; row++) {    
+    for(int col = 0; col < board_size; col++) {
+      gtp_printf("%d ", p[row][col]);
+    }
+    gtp_printf("\n");
+  }
   return gtp_finish_response();
 }
 
